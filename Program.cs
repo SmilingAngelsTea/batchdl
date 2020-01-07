c6c6d01d30cca0ad7dbeb493577ac1e3ce2c4bb5
SmilingAngelsTea <smilingangelstea@hotmail.com>
2020-01-07 17:45:45 +0300

    Usage text, folder path fix for the previous commit and exception prints

--- Program.cs
+++ Program.cs
@@ -5,17 +5,21 @@ using System.Net;
 namespace batchdl {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("batchdl\n\n" +
+                "Treats each line from a text file as a link and downloads them in order.\n" +
+                "USAGE: batchdl (text file location) [folder to output to]\n");
             if (args.Length < 1) {
                 Console.WriteLine("No text file specified. Exiting now.");
                 Environment.Exit(0);
             }
             string list = args[0];
-            string folder = "";
+            string folder;
             if (args.Length < 2) {
                 string listname = Path.GetFileNameWithoutExtension(list);
-                Console.WriteLine("No folder name specified. Using the text file name (" + listname + ") as the folder name.");
+                Console.WriteLine("No folder name specified. Using the text file name (" + listname + ") as the folder name.\n" +
+                "Do note that the folder will be created next to the text file.");
                 folder = listname;
-                Environment.Exit(0);
+                Console.WriteLine(folder);
             } else {
                 folder = args[1];
             }
@@ -29,11 +33,13 @@ namespace batchdl {
                     var extension = Path.GetExtension(uri.AbsolutePath);
                     Console.WriteLine("Downloading: " + urls[i] + " as " + i + extension);
                     WebClient webClient = new WebClient();
-                    webClient.DownloadFile(urls[i], folder + "//" + i + extension);
+                    Console.WriteLine(Path.GetDirectoryName(list) + "//" + folder);
+                    webClient.DownloadFile(urls[i], Path.GetDirectoryName(list) + "//" + folder + "//" + i + extension);
                 }
 
-            } catch {
-                Console.WriteLine("Something went wrong.");
+            } catch (Exception e){
+                Console.WriteLine("\nEXCEPTION:");
+                Console.WriteLine(e.ToString());
                 Environment.Exit(0);
             }
         }
